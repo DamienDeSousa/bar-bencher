888b23e80ffb2ef0cca7d398a4d21e2b0398067f
DamienDeSousa <desousadamien30@gmail.com>
2024-08-10 20:26:11 +0200

    update for specific day

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
       name: "Mint Julep",
       ingredients: ["bourbon", "menthe", "sirop de sucre"],
     },
-    {
-      name: "Vodkatini",
-      ingredients: ["vodka", "vermouth blanc", "olives (en option)"],
-    },
+    // {
+    //   name: "Vodkatini",
+    //   ingredients: ["vodka", "vermouth blanc", "olives (en option)"],
+    // },
     {
       name: "Rhum Collins",
       ingredients: [
@@ -52,18 +52,18 @@ export default function Home() {
         "menthe",
       ],
     },
-    {
-      name: "Vesper",
-      ingredients: ["gin", "vodka", "lillet"],
-    },
+    // {
+    //   name: "Vesper",
+    //   ingredients: ["gin", "vodka", "lillet"],
+    // },
     {
       name: "Old Fashioned",
       ingredients: ["sucre", "angostura", "bourbon", "zeste d'orange"],
     },
-    {
-      name: "Stinger",
-      ingredients: ["cognac", "crème de menthe blanche"],
-    },
+    // {
+    //   name: "Stinger",
+    //   ingredients: ["cognac", "crème de menthe blanche"],
+    // },
     {
       name: "Cuba Libre",
       ingredients: ["rhum blanc", "coca", "citron vert"],
